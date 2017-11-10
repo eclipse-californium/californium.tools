feb91a194ce2208765a45dd43554058641eea2da
Kai Hudalla <kai.hudalla@bosch-si.com>
2017-11-10 13:07:37 +0100

    [#398] Migrate cf-browser to Cf 2.0.x and SLF4J.

--- cf-browser/src/main/java/org/eclipse/californium/tools/GUIClientFX.java
+++ cf-browser/src/main/java/org/eclipse/californium/tools/GUIClientFX.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Red Hat
+ * Copyright (c) 2015, 2017 Institute for Pervasive Computing, ETH Zurich and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,51 +13,51 @@
  * Contributors:
  *    Matthias Kovatsch - creator and main architect
  *    Martin Lanter - architect and initial implementation
+ *    Bosch Software Innovations GmbH - migrate to SLF4J
  ******************************************************************************/
 package org.eclipse.californium.tools;
 
 import java.io.PrintStream;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.eclipse.californium.core.CaliforniumLogger;
 
 /**
  * A JavaFX CoAP Client to communicate with other CoAP resources.
  */
 public class GUIClientFX extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	private static final Logger LOG = LoggerFactory.getLogger(GUIClientFX.class);
 
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        URL fxml = getClass().getResource("gui.fxml");
-        FXMLLoader loader = new FXMLLoader(fxml);
-        Parent root = loader.load();
-        GUIController controller = loader.getController();
-        PrintStream ps = new PrintStream(controller.getLogStream());
-        System.setOut(ps);
-        System.setErr(ps);
-        CaliforniumLogger.initialize();
-        CaliforniumLogger.setLevel(Level.FINE);
-        Logger log = Logger.getLogger(GUIClientFX.class.getName());
-        log.info(String.format("MainFX.controller=%s\n", controller));
-        primaryStage.setTitle("CoAP Client");
-        primaryStage.setScene(new Scene(root, 900, 650));
-        primaryStage.show();
-    }
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		URL fxml = getClass().getResource("gui.fxml");
+		FXMLLoader loader = new FXMLLoader(fxml);
+		Parent root = loader.load();
+		GUIController controller = loader.getController();
+		PrintStream ps = new PrintStream(controller.getLogStream());
+		System.setOut(ps);
+		System.setErr(ps);
+		LOG.info("MainFX.controller={}", controller);
+		primaryStage.setTitle("CoAP Client");
+		primaryStage.setScene(new Scene(root, 900, 650));
+		primaryStage.show();
+	}
 
 }
--- cf-browser/src/main/java/org/eclipse/californium/tools/GUIController.java
+++ cf-browser/src/main/java/org/eclipse/californium/tools/GUIController.java
@@ -1,6 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Institute for Pervasive Computing, ETH Zurich and others.
- * Copyright (c) 2015 Red Hat
+ * Copyright (c) 2015, 2017 Institute for Pervasive Computing, ETH Zurich and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,6 +13,7 @@
  * Contributors:
  *    Matthias Kovatsch - creator and main architect
  *    Martin Lanter - architect and initial implementation
+ *    Bosch Software Innovations GmbH - migrate to SLF4J
  ******************************************************************************/
 package org.eclipse.californium.tools;
 
@@ -24,15 +24,22 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.InetAddress;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.coap.MessageObserverAdapter;
+import org.eclipse.californium.core.coap.OptionSet;
+import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.coap.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -47,257 +54,273 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import org.eclipse.californium.core.CaliforniumLogger;
-import org.eclipse.californium.core.coap.CoAP;
-import org.eclipse.californium.core.coap.MediaTypeRegistry;
-import org.eclipse.californium.core.coap.MessageObserverAdapter;
-import org.eclipse.californium.core.coap.OptionSet;
-import org.eclipse.californium.core.coap.Request;
-import org.eclipse.californium.core.coap.Response;
 
 /**
- * The JavaFX controller for the gui.fxml template
- * TODO: coaps support
- * TODO: better display of different media types
+ * The JavaFX controller for the gui.fxml template TODO: coaps support TODO:
+ * better display of different media types
  */
 public class GUIController {
-    private static Logger log = Logger.getLogger(GUIController.class.getName());
-    private static final String DEFAULT_URI = "coap://localhost:5683";
-    private static final String COAP_PROTOCOL = "coap://";
 
-    /** Combo boxes of coap URIs and resource URIs of discovered servers */
-    @FXML
-    private ComboBox<String> uriBox;
-    @FXML
-    private TextArea logArea;
-    @FXML
-    private CheckMenuItem logEnabled;
-    @FXML
-    private TextArea requestArea;
-    @FXML
-    private TextArea responseArea;
-    @FXML
-    private TitledPane responseTitle;
-    @FXML
-    private TreeView resourceTree;
-    @FXML
-    private ImageView mediaTypeView;
-    private String selectedURI;
-    private String coapHost;
+	private static final Logger LOG = LoggerFactory.getLogger(GUIController.class.getName());
+	private static final String DEFAULT_URI = "coap://localhost:5683";
+	private static final String COAP_PROTOCOL = "coap://";
 
-    public OutputStream getLogStream() {
-        return new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
-                throw new IOException("Not implemented");
-            }
+	/** Combo boxes of coap URIs and resource URIs of discovered servers */
+	@FXML
+	private ComboBox<String> uriBox;
+	@FXML
+	private TextArea logArea;
+	@FXML
+	private CheckMenuItem logEnabled;
+	@FXML
+	private TextArea requestArea;
+	@FXML
+	private TextArea responseArea;
+	@FXML
+	private TitledPane responseTitle;
+	@FXML
+	private TreeView resourceTree;
+	@FXML
+	private ImageView mediaTypeView;
+	private String selectedURI;
+	private String coapHost;
 
-            @Override
-            public void write(byte[] b, int off, int len) throws IOException {
-                String line = new String(b, off, len);
-                Platform.runLater(() -> logArea.appendText(line));
-            }
-        };
-    }
+	public OutputStream getLogStream() {
+		return new OutputStream() {
 
-    @FXML
-    private void initialize() {
-        uriBox.itemsProperty().get().add(DEFAULT_URI);
-        uriBox.getSelectionModel().select(0);
-        // Initialize the
-        InputStream imgIS = getClass().getResourceAsStream("/org/eclipse/californium/tools/images/unknown.png");
-        Image unknown = new Image(imgIS);
-        mediaTypeView.setImage(unknown);
-    }
+			@Override
+			public void write(int b) throws IOException {
+				throw new IOException("Not implemented");
+			}
 
-    @FXML
-    private void uriSelected() {
-        selectedURI = uriBox.getSelectionModel().getSelectedItem();
-    }
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException {
+				String line = new String(b, off, len);
+				Platform.runLater(() -> logArea.appendText(line));
+			}
+		};
+	}
 
-    @FXML
-    private void getRequest() {
-        performRequest(Request.newGet());
-    }
-    @FXML
-    private void postRequest() {
-        performRequest(Request.newPost());
-    }
-    @FXML
-    private void putRequest() {
-        performRequest(Request.newPut());
-    }
-    @FXML
-    private void deleteRequest() {
-        performRequest(Request.newPost());
-    }
+	@FXML
+	private void initialize() {
+		uriBox.itemsProperty().get().add(DEFAULT_URI);
+		uriBox.getSelectionModel().select(0);
+		// Initialize the
+		InputStream imgIS = getClass().getResourceAsStream("/org/eclipse/californium/tools/images/unknown.png");
+		Image unknown = new Image(imgIS);
+		mediaTypeView.setImage(unknown);
+	}
 
-    @FXML
-    private void discoveryRequest() {
-        Request request = new Request(CoAP.Code.GET);
-        coapHost = getHost();
-        request.setURI(COAP_PROTOCOL + coapHost + "/.well-known/core");
-        log.info("Begin discovery, host="+coapHost);
-        request.addMessageObserver(new MessageObserverAdapter() {
-            @SuppressWarnings({"rawtypes", "unchecked"})
-            public void onResponse(Response response) {
-                log.info(String.format("Discovery, response: %s", response));
-                String text = response.getPayloadString();
-                Scanner scanner = new Scanner(text);
-                Pattern pattern = Pattern.compile("<");
-                scanner.useDelimiter(pattern);
+	@FXML
+	private void uriSelected() {
+		selectedURI = uriBox.getSelectionModel().getSelectedItem();
+	}
 
-                ObservableList<String> ress1 = FXCollections.observableArrayList();
-                ArrayList<String> ress2 = new ArrayList<>();
-                while (scanner.hasNext()) {
-                    String part = scanner.next();
-                    String res = part.split(">")[0];
-                    log.info(res);
-                    ress1.add(COAP_PROTOCOL + coapHost + res);
-                    ress2.add(res);
-                }
-                scanner.close();
-                uriBox.itemsProperty().setValue(ress1);
-                Platform.runLater(() -> populateTree(ress2));
-            }
-        });
-        execute(request);
-    }
+	@FXML
+	private void getRequest() {
+		performRequest(Request.newGet());
+	}
 
-    @FXML
-    private void onSelectResource() {
-        TreeItem<String> item = (TreeItem<String>) resourceTree.getSelectionModel().getSelectedItem();
-        StringBuilder path = new StringBuilder(item.getValue());
-        while(item.getParent() != null) {
-            item = item.getParent();
-            path.insert(0, item.getValue());
-        }
-        // Prepend the coap uri
-        path.insert(0, COAP_PROTOCOL+coapHost);
-        log.info("selected resource: "+path);
-        uriBox.getSelectionModel().select(path.toString());
-    }
+	@FXML
+	private void postRequest() {
+		performRequest(Request.newPost());
+	}
 
-    @FXML
-    private void toggleLogging() {
-        if(logEnabled.isSelected()) {
-            CaliforniumLogger.setLevel(Level.FINE);
-        } else {
-            CaliforniumLogger.disableLogging();
-            clearLog();
-        }
-    }
+	@FXML
+	private void putRequest() {
+		performRequest(Request.newPut());
+	}
 
-    @FXML
-    private void clearLog() {
-        Platform.runLater(logArea::clear);
-    }
-    @FXML
-    private void setLogLevel(ActionEvent event) {
-        MenuItem item = (MenuItem) event.getSource();
-        String text = item.getText();
-        Level level = Level.parse(text);
-        CaliforniumLogger.setLevel(level);
-    }
+	@FXML
+	private void deleteRequest() {
+		performRequest(Request.newPost());
+	}
 
-    private String getHost() {
-        String uri = uriBox.getSelectionModel().getSelectedItem();
-        StringTokenizer st = new StringTokenizer(uri, "/");
-        st.nextToken();
-        String host = st.nextToken();
-        return host;
-    }
+	@FXML
+	private void discoveryRequest() {
+		Request request = new Request(CoAP.Code.GET);
+		coapHost = getHost();
+		request.setURI(COAP_PROTOCOL + coapHost + "/.well-known/core");
+		LOG.info("Begin discovery, host={}", coapHost);
+		request.addMessageObserver(new MessageObserverAdapter() {
 
-    private void populateTree(List<String> ress) {
-        TreeItem<String> rootItem = new TreeItem<>("/");
-        for(String res : ress) {
-            String[] parts = res.split("/");
-            TreeItem cur = rootItem;
-            for (int i=1;i<parts.length;i++) {
-                TreeItem search = new TreeItem(parts[i]);
-                ObservableList<TreeItem<String>> children = cur.getChildren();
-                int index = children.indexOf(search);
-                if(index < 0) {
-                    children.add(search);
-                    cur = search;
-                }
-                else {
-                    cur = children.get(index);
-                }
-            }
-        }
-        rootItem.setExpanded(true);
-        resourceTree.setRoot(rootItem);
-    }
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			public void onResponse(Response response) {
+				LOG.info("Discovery, response: {}", response);
+				String text = response.getPayloadString();
+				Scanner scanner = new Scanner(text);
+				Pattern pattern = Pattern.compile("<");
+				scanner.useDelimiter(pattern);
 
-    /**
-     * Perform the given request by adding in the resource uri from the uri combo box selection,
-     * payload from the request text area, and message observer to a ResponsePrinter.
-     * @param request - the coap request type
-     */
-    private void performRequest(Request request) {
-        responseArea.setText("no response yet");
-        responseTitle.setText("Response: none");
-        request.addMessageObserver(new ResponsePrinter());
-        request.setPayload(requestArea.getText());
-        String uri = uriBox.getSelectionModel().getSelectedItem();
-        uri = uri.replace(" ", "%20");
-        request.setURI(uri);
-        execute(request);
-    }
+				ObservableList<String> ress1 = FXCollections.observableArrayList();
+				ArrayList<String> ress2 = new ArrayList<>();
+				while (scanner.hasNext()) {
+					String part = scanner.next();
+					String res = part.split(">")[0];
+					LOG.info(res);
+					ress1.add(COAP_PROTOCOL + coapHost + res);
+					ress2.add(res);
+				}
+				scanner.close();
+				uriBox.itemsProperty().setValue(ress1);
+				Platform.runLater(() -> populateTree(ress2));
+			}
+		});
+		execute(request);
+	}
 
-    private void execute(Request request) {
-        try {
-            request.send();
-            log.info("Sent request: "+request);
-        } catch (Exception ex) {
-            StringBuilder tmp = new StringBuilder(request.toString());
-            tmp.append('\n');
-            StringWriter sw = new StringWriter();
-            ex.printStackTrace(new PrintWriter(sw));
-            tmp.append(sw.toString());
-            log.severe(tmp.toString());
-        }
-    }
+	@FXML
+	private void onSelectResource() {
+		TreeItem<String> item = (TreeItem<String>) resourceTree.getSelectionModel().getSelectedItem();
+		StringBuilder path = new StringBuilder(item.getValue());
+		while (item.getParent() != null) {
+			item = item.getParent();
+			path.insert(0, item.getValue());
+		}
+		// Prepend the coap uri
+		path.insert(0, COAP_PROTOCOL + coapHost);
+		LOG.info("selected resource: {}", path);
+		uriBox.getSelectionModel().select(path.toString());
+	}
 
-    private class ResponsePrinter extends MessageObserverAdapter {
-        @Override
-        public void onResponse(final Response response) {
-            Platform.runLater(() -> {
-                log.info("Recevied response: "+response);
-                int size = response.getPayloadSize();
-                byte[] payload = response.getPayload();
-                CoAP.Type type = response.getType();
-                InetAddress source = response.getSource();
-                OptionSet optionSet = response.getOptions();
-                int format = optionSet.getContentFormat();
-                String mediaType = MediaTypeRegistry.toString(format);
-                if(mediaType.startsWith("image")) {
-                    // Display the image
-                    Image img = new Image(new ByteArrayInputStream(payload));
-                    mediaTypeView.setImage(img);
-                    // Display the image uri if given, otherwise just the type and size
-                    String uriPath = optionSet.getUriPathString();
-                    if(uriPath != null && uriPath.length() > 0)
-                        responseArea.setText(uriPath);
-                    else
-                        responseArea.setText(String.format("%s;size=%d", mediaType, size));
-                } else {
-                    // Display media type image icon and payload string
-                    String ext = MediaTypeRegistry.toFileExtension(format);
-                    String path = "/org/eclipse/californium/tools/images/"+ext+".png";
-                    InputStream imgIS = getClass().getResourceAsStream(path);
-                    if(imgIS == null)
-                        imgIS = getClass().getResourceAsStream("/org/eclipse/californium/tools/images/blank.png");
-                    Image unknown = new Image(imgIS);
-                    mediaTypeView.setImage(unknown);
-                    responseArea.setText(response.getPayloadString());
-                }
-                String info = String.format("Response: %s/%s;size=%d,mid=%d,source=%s,mediaType=%s", response.getCode(), response.getCode().name(),
-                        size, response.getMID(), source, mediaType);
-                responseTitle.setText(info);
-            });
-        }
-    }
+	@FXML
+	private void toggleLogging() {
+		Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		if (ch.qos.logback.classic.Logger.class.isInstance(logger)) {
+			ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger) logger;
+			if (logEnabled.isSelected()) {
+				setRootLoggerLevel(Level.DEBUG);
+			} else {
+				setRootLoggerLevel(Level.OFF);
+			}
+		}
+	}
+
+	@FXML
+	private void clearLog() {
+		Platform.runLater(logArea::clear);
+	}
+
+	@FXML
+	private void setLogLevel(ActionEvent event) {
+		MenuItem item = (MenuItem) event.getSource();
+		String text = item.getText();
+		Level level = Level.toLevel(text);
+		setRootLoggerLevel(level);
+	}
+
+	private void setRootLoggerLevel(Level newLevel) {
+		Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+		if (ch.qos.logback.classic.Logger.class.isInstance(logger)) {
+			ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger) logger;
+			rootLogger.setLevel(newLevel);
+			if (Level.OFF.equals(newLevel)) {
+				clearLog();
+			}
+		}
+	}
+
+	private String getHost() {
+		String uri = uriBox.getSelectionModel().getSelectedItem();
+		StringTokenizer st = new StringTokenizer(uri, "/");
+		st.nextToken();
+		String host = st.nextToken();
+		return host;
+	}
+
+	private void populateTree(List<String> ress) {
+		TreeItem<String> rootItem = new TreeItem<>("/");
+		for (String res : ress) {
+			String[] parts = res.split("/");
+			TreeItem cur = rootItem;
+			for (int i = 1; i < parts.length; i++) {
+				TreeItem search = new TreeItem(parts[i]);
+				ObservableList<TreeItem<String>> children = cur.getChildren();
+				int index = children.indexOf(search);
+				if (index < 0) {
+					children.add(search);
+					cur = search;
+				} else {
+					cur = children.get(index);
+				}
+			}
+		}
+		rootItem.setExpanded(true);
+		resourceTree.setRoot(rootItem);
+	}
+
+	/**
+	 * Perform the given request by adding in the resource uri from the uri
+	 * combo box selection, payload from the request text area, and message
+	 * observer to a ResponsePrinter.
+	 * 
+	 * @param request - the coap request type
+	 */
+	private void performRequest(Request request) {
+		responseArea.setText("no response yet");
+		responseTitle.setText("Response: none");
+		request.addMessageObserver(new ResponsePrinter());
+		request.setPayload(requestArea.getText());
+		String uri = uriBox.getSelectionModel().getSelectedItem();
+		uri = uri.replace(" ", "%20");
+		request.setURI(uri);
+		execute(request);
+	}
+
+	private void execute(Request request) {
+		try {
+			request.send();
+			LOG.info("Sent request: {}", request);
+		} catch (Exception ex) {
+			StringBuilder tmp = new StringBuilder(request.toString());
+			tmp.append('\n');
+			StringWriter sw = new StringWriter();
+			ex.printStackTrace(new PrintWriter(sw));
+			tmp.append(sw.toString());
+			LOG.error(tmp.toString());
+		}
+	}
+
+	private class ResponsePrinter extends MessageObserverAdapter {
+
+		@Override
+		public void onResponse(final Response response) {
+			Platform.runLater(() -> {
+				LOG.info("Received response: {}", response);
+				int size = response.getPayloadSize();
+				byte[] payload = response.getPayload();
+				CoAP.Type type = response.getType();
+				InetAddress source = response.getSource();
+				OptionSet optionSet = response.getOptions();
+				int format = optionSet.getContentFormat();
+				String mediaType = MediaTypeRegistry.toString(format);
+				if (mediaType.startsWith("image")) {
+					// Display the image
+					Image img = new Image(new ByteArrayInputStream(payload));
+					mediaTypeView.setImage(img);
+					// Display the image uri if given, otherwise just the type
+					// and size
+					String uriPath = optionSet.getUriPathString();
+					if (uriPath != null && uriPath.length() > 0)
+						responseArea.setText(uriPath);
+					else
+						responseArea.setText(String.format("%s;size=%d", mediaType, size));
+				} else {
+					// Display media type image icon and payload string
+					String ext = MediaTypeRegistry.toFileExtension(format);
+					String path = "/org/eclipse/californium/tools/images/" + ext + ".png";
+					InputStream imgIS = getClass().getResourceAsStream(path);
+					if (imgIS == null)
+						imgIS = getClass().getResourceAsStream("/org/eclipse/californium/tools/images/blank.png");
+					Image unknown = new Image(imgIS);
+					mediaTypeView.setImage(unknown);
+					responseArea.setText(response.getPayloadString());
+				}
+				String info = String.format("Response: %s/%s;size=%d,mid=%d,source=%s,mediaType=%s", response.getCode(),
+						response.getCode().name(), size, response.getMID(), source, mediaType);
+				responseTitle.setText(info);
+			});
+		}
+	}
 
 }
