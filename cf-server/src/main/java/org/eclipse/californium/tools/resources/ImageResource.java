047f5e05bb3ebf89ac3cf06249646519e001329f
Achim Kraus <achim.kraus@bosch-si.com>
2019-11-15 16:25:35 +0100

    Replace deprecated image types with octet stream.
    Issue #1078
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- cf-server/src/main/java/org/eclipse/californium/tools/resources/ImageResource.java
+++ cf-server/src/main/java/org/eclipse/californium/tools/resources/ImageResource.java
@@ -36,29 +36,28 @@ public class ImageResource extends CoapResource {
 
 	private String filePath = "src/main/resources/data/image/";
 	private String fileName = "image";
-	
+
 	private List<Integer> supported = new ArrayList<Integer>();
 
 	public ImageResource(String resourceIdentifier) {
 		super(resourceIdentifier);
-		
+
 		ResourceAttributes attributes = getAttributes();
 		attributes.setTitle("GET an image with different content-types");
 		attributes.addResourceType("Image");
-		
-		supported.add(MediaTypeRegistry.IMAGE_PNG);
-		supported.add(MediaTypeRegistry.IMAGE_JPEG);
-		
+
+		supported.add(MediaTypeRegistry.APPLICATION_OCTET_STREAM);
+
 		for (int ct : supported) {
 			attributes.addContentType(ct);
 		}
-		
+
 		attributes.setMaximumSizeEstimate(18029);
 	}
 
 	@Override
 	public void handleGET(CoapExchange exchange) {
-		Integer ct = MediaTypeRegistry.IMAGE_PNG;
+		Integer ct = MediaTypeRegistry.APPLICATION_OCTET_STREAM;
 		if (exchange.getRequestOptions().hasAccept()) {
 			ct = exchange.getRequestOptions().getAccept();
 			if (!supported.contains(ct)) {
@@ -66,30 +65,30 @@ public class ImageResource extends CoapResource {
 				return;
 			}
 		}
-		
+
 		String filename = filePath + fileName + "." + MediaTypeRegistry.toFileExtension(ct);
 
 		// load representation from file
 		System.out.println("Opening file "+filename+", "+new File(filename).getAbsolutePath());
 		File file = new File(filename);
-		
+
 		if (!file.exists()) {
 			exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR, "Image file not found");
 			return;
 		}
-		
+
 		// get length of file for buffer
-        int fileLength = (int)file.length();
-        byte[] fileData = new byte[fileLength];
-        
-        try
-        {
+		int fileLength = (int)file.length();
+		byte[] fileData = new byte[fileLength];
+
+		try
+		{
 			// open input stream from file
-        	FileInputStream fileIn = new FileInputStream(file);
+			FileInputStream fileIn = new FileInputStream(file);
 			// read file into byte array
 			fileIn.read(fileData);
 			fileIn.close();
-			
+
 			// create response
 			Response response = new Response(ResponseCode.CONTENT);
 			response.setPayload(fileData);
@@ -97,9 +96,9 @@ public class ImageResource extends CoapResource {
 
 			exchange.respond(response);
 			
-        } catch (Exception e) {
+		} catch (Exception e) {
 			exchange.respond(ResponseCode.INTERNAL_SERVER_ERROR, "I/O error");
-        }
+		}
 	}
-	
+
 }
